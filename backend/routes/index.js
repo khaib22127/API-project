c8f660ed9487c5a7915610e92a56bfe75e9adad3
khai bui <khaib22127@gmail.com>
2022-12-14 15:17:44 -0700

    Add error-handling middleware for common errors and debugging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 
 router.get("/api/csrf/restore", (req, res) => {
